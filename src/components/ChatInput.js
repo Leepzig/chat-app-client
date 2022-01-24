a178bbd6e199d291749f7d50bb3d2cc329b6992e
andrewsimmons <neon.duncan@gmail.com>
2022-01-24 10:12:03 -0600

    cleaned up chatInput, removed a conditional that wasn't needed anymore

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,50 +1,46 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState } from 'react';
 
 const ChatInput = ({ socket, addMessage, currentUser }) => {
-    const [msgForm, setMsgForm] = useState("")
+  const [msgForm, setMsgForm] = useState("")
 
-    const getTimeStamp = () => {
-      const date = new Date()
-      const hours = date.getHours().toString()
-      const minutes = date.getMinutes().toString()
-      const seconds = date.getSeconds().toString()
-      return `${hours}:${minutes}:${seconds}` 
+  const getTimeStamp = () => {
+    const date = new Date()
+    const hours = date.getHours().toString()
+    const minutes = date.getMinutes().toString()
+    const seconds = date.getSeconds().toString()
+    return `${hours}:${minutes}:${seconds}` 
+  }
 
+  const handleChange = e => {
+      setMsgForm(e.target.value)
     }
+  
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (msgForm) {
+      const msgObject = {user:currentUser.username, content: msgForm, time: getTimeStamp()}
+      socket.emit('send message', msgObject)
+      setMsgForm("")
+      sendMessageToDB(msgObject)
+    }
+  }
+  const sendMessageToDB = msg => {
+    const options = {
+      method:'POST',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(msg)
+    }
+    fetch('http://localhost:3001/messages', options)
+    .then(response => response.json())
+    .then(data => addMessage(data))
+  }
 
-    const handleChange = e => {
-        setMsgForm(e.target.value)
-      }
-    
-      //TODO Clean this up
-      const handleSubmit = e => {
-        e.preventDefault()
-        if (msgForm) {
-          //TODO time should be a helper
-          const msgObject = {user:currentUser.username, content: msgForm, time: getTimeStamp()}
-          socket.emit('send message', msgObject)
-          setMsgForm("")
-          sendMessageToDB(msgObject)
-        }
-      }
-      const sendMessageToDB = msg => {
-        const options = {
-          method:'POST',
-          headers:{
-            'Accept':'application/json',
-            'Content-Type':'application/json'
-          },
-          body:JSON.stringify(msg)
-        }
-        fetch('http://localhost:3001/messages', options)
-        .then(response => response.json())
-        .then(data => addMessage(data))
-      }
-
-      //TODO: Can we find another way to do this?
-      if (socket) {socket.on('sent message', (message) => {
-        addMessage(message)
-      })}
+  socket.on('sent message', (message) => {
+      addMessage(message)
+    })
    
   return (
   <div>
