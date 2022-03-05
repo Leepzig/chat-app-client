ed73b19e7077a4104288354a6d1caebf91f8a20c
andrewsimmons <neon.duncan@gmail.com>
2022-03-05 11:47:48 -0600

    added auto scrolling to messages box component

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,8 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import ChatInput from './ChatInput';
+
+import { Container, Box, Typography } from '@mui/material';
 
 const Messages = ({ socket, currentUser }) => {
     const [messages, setMessages] = useState([])
+    const chatBox = useRef()
     
     const addMessage = msg => {
         setMessages([...messages, msg])
@@ -23,11 +26,19 @@ const Messages = ({ socket, currentUser }) => {
         loadMessages()
     },[])
 
+    useEffect(() => {
+        chatBox.current.scrollTop = chatBox.current.scrollHeight
+    }, [messages])
+
   return (
-  <div>
-      {messages.map(msg => <p key={msg.content}>{`${msg.user} ${msg.time} ${msg.content}`}</p>)}
-      <ChatInput socket={socket} addMessage={addMessage} currentUser={currentUser}/>
-  </div>
+      <>
+        <Container>
+            <Box id="chatbox" sx={{ height:"300px", my: 2, overflow:"auto", border:"solid" }} ref={chatBox}>
+                {messages.map(msg => <Typography variant="body1" key={msg.content}>{`${msg.user} ${msg.time} ${msg.content}`}</Typography>)}
+            </Box>
+        <ChatInput socket={socket} addMessage={addMessage} currentUser={currentUser}/>
+      </Container>
+      </>
   );
 };
 
